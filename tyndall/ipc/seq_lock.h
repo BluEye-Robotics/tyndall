591fe2b731e5ce6885cb4d8aa68597987d1ee1a3
Johannes Schrimpf <johannes.schrimpf@blueye.no>
2024-11-18 12:54:39 +0100

    Format seq_lock.h

--- tyndall/ipc/seq_lock.h
+++ tyndall/ipc/seq_lock.h
@@ -1,9 +1,9 @@
 /*
 seq_lock.h
 
-Sequence lock: https://en.wikipedia.org/wiki/Seqlock, https://lwn.net/Articles/21355/
-Supports single writer, multiple readers
-Reader always gets the most recent entry
+Sequence lock: https://en.wikipedia.org/wiki/Seqlock,
+https://lwn.net/Articles/21355/ Supports single writer, multiple readers Reader
+always gets the most recent entry
 
 Inspired by:
 http://www.1024cores.net/home/lock-free-algorithms/reader-writer-problem/improved-lock-free-seqlock
@@ -12,12 +12,12 @@ https://github.com/rigtorp/Seqlock
 */
 
 #pragma once
+#include "smp.h"
 #include <assert.h>
 #include <errno.h>
-#include "smp.h"
 
-__attribute__((always_inline)) static inline unsigned seq_lock_write_begin(unsigned* seq)
-{
+__attribute__((always_inline)) static inline unsigned
+seq_lock_write_begin(unsigned *seq) {
   unsigned seq1 = *seq;
 
   smp_write_once(*seq, ++seq1);
@@ -26,16 +26,16 @@ __attribute__((always_inline)) static inline unsigned seq_lock_write_begin(unsig
   return seq1;
 }
 
-__attribute__((always_inline)) static inline void seq_lock_write_end(unsigned* seq, unsigned seq1)
-{
+__attribute__((always_inline)) static inline void
+seq_lock_write_end(unsigned *seq, unsigned seq1) {
   smp_wmb();
   smp_write_once(*seq, ++seq1);
 }
 
-__attribute__((always_inline)) static inline unsigned seq_lock_read_begin(unsigned* seq)
-{
+__attribute__((always_inline)) static inline unsigned
+seq_lock_read_begin(unsigned *seq) {
   unsigned seq1;
-  while((seq1 = smp_read_once(*seq)) & 1)
+  while ((seq1 = smp_read_once(*seq)) & 1)
     cpu_relax();
 
   smp_rmb();
@@ -43,27 +43,22 @@ __attribute__((always_inline)) static inline unsigned seq_lock_read_begin(unsign
   return seq1;
 }
 
-__attribute__((always_inline)) static inline unsigned seq_lock_read_retry(unsigned* seq, unsigned seq1)
-{
+__attribute__((always_inline)) static inline unsigned
+seq_lock_read_retry(unsigned *seq, unsigned seq1) {
   smp_rmb();
   unsigned seq2 = smp_read_once(*seq);
 
   return seq2 != seq1;
 }
 
-
 #ifdef __cplusplus
 
-
-struct seq_lock_state
-{
+struct seq_lock_state {
   unsigned prev_seq = 0;
   bool has_read_once = false;
 };
 
-template<typename STORAGE>
-struct seq_lock
-{
+template <typename STORAGE> struct seq_lock {
   static_assert(std::is_nothrow_copy_assignable_v<STORAGE>);
   static_assert(std::is_trivially_copy_assignable_v<STORAGE>);
 
@@ -74,12 +69,10 @@ struct seq_lock
   STORAGE entry;
 
 public:
-
   using storage = STORAGE;
   using state = seq_lock_state;
 
-  void write(const STORAGE& entry, seq_lock_state&)
-  {
+  void write(const STORAGE &entry, seq_lock_state &) {
     unsigned seq = seq_lock_write_begin(&this->seq);
 
     this->entry = entry;
@@ -87,37 +80,30 @@ public:
     seq_lock_write_end(&this->seq, seq);
   }
 
-  int read(STORAGE& entry, seq_lock_state& state)
-  {
+  int read(STORAGE &entry, seq_lock_state &state) {
     int rc;
     unsigned seq1;
 
-    do
-    {
+    do {
       seq1 = seq_lock_read_begin(&this->seq);
       entry = this->entry;
 
     } while (seq_lock_read_retry(&this->seq, seq1));
 
-    if (seq1 != state.prev_seq)
-    {
+    if (seq1 != state.prev_seq) {
       rc = 0;
       state.prev_seq = seq1;
       if (!state.has_read_once)
         state.has_read_once = true;
-    }
-    else if ((seq1 == 0) && !state.has_read_once)
-    {
+    } else if ((seq1 == 0) && !state.has_read_once) {
       rc = -1;
       errno = ENOMSG;
-    }
-    else
-    {
+    } else {
       rc = -1;
       errno = EAGAIN;
     }
     return rc;
   }
-} __attribute__ ((aligned(CACHELINE_BYTES)));
+} __attribute__((aligned(CACHELINE_BYTES)));
 
 #endif
